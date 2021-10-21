29d9dce8dbdab598228264e2c41573f77e67d953
Ramon William <roniewill@gmail.com>
2021-10-20 22:17:33 -0300

    add admin sign in and sign out

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+
+  context 'check if admin is valid' do
+    admin = Admin.new(email: "admin@email.com", password: "123456")
+
+    it 'valid admin email' do
+      expect(admin.email).to eql 'admin@email.com'
+    end
+
+    it 'valid password admin' do
+      expect(admin.password).to eql '123456'
+    end
+
+    it 'admin is valid' do
+      expect(admin).to be_valid
+    end
+  end
+  
+  context 'check if admin is not valid' do
+    admin = Admin.new(email: "admin@email.com", password: "")
+
+    it 'password is invalid because is empty' do
+      expect(admin.password).to be_empty
+    end
+
+    it 'admin is not valid' do
+      expect(admin).to_not be_valid
+    end
+  end
+
+end
